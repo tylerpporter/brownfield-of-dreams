7a379a8a2da9a75ac0554ae6f22e536e5beaea69
Raymond Nguyen <rayknguyen@gmail.com>
2020-05-05 14:42:16 -0700

    Add test for followers to dashboard
    Co-authored-by: Tyler Porter <tylerpporter@gmail.com>

--- spec/features/user/user_can_see_github_section_spec.rb
+++ spec/features/user/user_can_see_github_section_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'As a logged in user' do
   describe 'When I visit /dashboard' do
-    it 'I see a Github section with a list of 5 of my repositories if I have a token' do
+    it 'see a Github section with a list of 5 of my repositories if I have a token' do
       user = create(:user, token: "token #{ENV['GITHUB_TOKEN']}")
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       visit dashboard_path
@@ -11,7 +11,7 @@ describe 'As a logged in user' do
       expect(page).to have_content("Repos")
       expect(page).to have_css('.repos', count: 5)
     end
-    it 'I do not see a Github section if I do not have a token' do
+    it 'does not see a Github section if I do not have a token' do
       user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       visit dashboard_path
@@ -19,6 +19,21 @@ describe 'As a logged in user' do
       expect(page).to_not have_content("Github")
       expect(page).to_not have_content("Repos")
     end
-    
+    it 'sees a section called followers and a list of the users followers' do
+      user = create(:user, token: "token #{ENV['GITHUB_TOKEN']}")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit dashboard_path
+
+      expect(page).to have_content("Github")
+      expect(page).to have_content("Followers")
+    end    
+    it 'sees a section called followers and a list of the users followers' do
+      user = create(:user, token: "token #{ENV['GITHUB_TOKEN']}")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit dashboard_path
+
+      expect(page).to have_content("Github")
+      expect(page).to have_content("Followings")
+    end
   end
 end

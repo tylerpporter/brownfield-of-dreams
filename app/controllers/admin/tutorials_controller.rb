4a16b1c66345648d0776a41a0ef87290343ff96c
Raymond Nguyen <rayknguyen@gmail.com>
2020-05-07 16:28:13 -0700

    Refactor to private method

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -8,18 +8,16 @@ class Admin::TutorialsController < Admin::BaseController
     playlist = youtube.playlist(params[:playlist_id])
     tutorial = Tutorial.create(import_tutorial_params)
     playlist[:items].each do |video|
-      vid = PlaylistVideo.new(video)
-      new_video_params = {title: vid.title, description: vid.description, video_id: vid.id, thumbnail: vid.thumbnail}
-      video = tutorial.videos.create(new_video_params)
+      create_video(video, tutorial)
     end
     flash[:success] = "Successfully created tutorial. #{view_context.link_to 'View it here', tutorial_path(tutorial.id)}.".html_safe
     redirect_to admin_dashboard_path
   end
-
+  
   def new
     @tutorial = Tutorial.new
   end
-
+  
   def update
     tutorial = Tutorial.find(params[:id])
     if tutorial.update(tutorial_params)
@@ -27,19 +25,25 @@ class Admin::TutorialsController < Admin::BaseController
     end
     redirect_to edit_admin_tutorial_path(tutorial)
   end
-
+  
   def destroy
     tutorial = Tutorial.find(params[:id])
     flash[:success] = "#{tutorial.title} tagged!" if tutorial.destroy
     redirect_to admin_dashboard_path
   end
-
+  
   private
-
+  
+  def create_video(video, tutorial)
+    vid = PlaylistVideo.new(video)
+    new_video_params = {title: vid.title, description: vid.description, video_id: vid.id, thumbnail: vid.thumbnail}
+    video = tutorial.videos.create(new_video_params)
+  end
+  
   def tutorial_params
     params.require(:tutorial).permit(:tag_list)
   end
-
+  
   def import_tutorial_params
     params.permit(:title, :desciption, :thumbnail, :playlist_id)
   end

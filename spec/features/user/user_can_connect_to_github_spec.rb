2ff7ca1e138bd2090e3e8079a39cd9ca9559975e
Tyler Porter <tylerpporter@gmail.com>
2020-05-08 20:14:12 -0600

    Working on test for omniauth

---
+++ spec/features/user/user_can_connect_to_github_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'As a user' , :vcr do
+  before do
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
+  end
+  scenario 'I can connect to Github' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit dashboard_path
+    click_button "Connect to Github"
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      provider: 'github',
+      uid: '12345',
+      token: ENV['GITHUB_TOKEN']
+      })
+    expect(user.uid).to eq('12345')
+  end
+end

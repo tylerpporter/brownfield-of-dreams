5b09deddabcb222ce716c93907f2a338b5ecc2c9
Tyler Porter <tylerpporter@gmail.com>
2020-05-11 19:44:38 -0600

    Create friendship model methods for creating and destroying reciprocals

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -5,4 +5,29 @@ describe Friendship do
     it {should belong_to :user}
     it {should belong_to :friend}
   end
+  describe 'class methods' do
+    it 'create_reciprocal' do
+      user1 = create(:user)
+      user2 = create(:user)
+      Friendship.create_reciprocal(user1.id, user2.id)
+      friendship = Friendship.last
+
+      expect(user1.friendships.size).to eq(1)
+      expect(user2.friendships.size).to eq(1)
+      expect(Friendship.all.size).to eq(2)
+      expect(friendship.user_id).to eq(user2.id)
+      expect(friendship.friend_id).to eq(user1.id)
+    end
+    it 'destroy_reciprocal' do
+      user1 = create(:user)
+      user2 = create(:user)
+      Friendship.create_reciprocal(user1.id, user2.id)
+
+      expect(Friendship.all.size).to eq(2)
+
+      Friendship.destroy_reciprocal(user1.id, user2.id)
+
+      expect(Friendship.all).to be_empty
+    end
+  end
 end

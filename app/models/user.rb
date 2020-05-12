a266a4712e8041300da2b6137ec99177fdfe896c
Tyler Porter <tylerpporter@gmail.com>
2020-05-11 19:33:45 -0600

    Create friendship table and relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_videos, dependent: :destroy
   has_many :videos, through: :user_videos
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
 
   validates :email, uniqueness: true, presence: true
   validates :password_digest, presence: true

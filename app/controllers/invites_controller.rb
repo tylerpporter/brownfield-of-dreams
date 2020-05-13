67fedcbfeed5cf32353f4ea5de24090d5830fb40
Tyler Porter <tylerpporter@gmail.com>
2020-05-13 12:04:20 -0600

    Refactor invites controller

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,13 +3,19 @@ class InvitesController < ApplicationController
 
   def create
     github_user = GithubService.get_user(params[:github_handle])
-    if github_user[:email]
-      ActivationMailer.invite(github_user, current_user).deliver_now
-      flash[:success] = "Successfully sent invite!"
-    else
-      flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
-    end
+    github_user[:email] ? send_invite(github_user) : failure
     redirect_to dashboard_path
+  end
+
+  private
+
+  def send_invite(github_user)
+    ActivationMailer.invite(github_user, current_user).deliver_now
+    flash[:success] = "Successfully sent invite!"
+  end
+
+  def failure
+    flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
   end
 
 end
